cd943a7a8a20e8f28f8ebafbb2ae5dfa80428392
Brayam Fajardo Velasquez <126234214+BrayamDev@users.noreply.github.com>
2024-11-13 00:53:29 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  "appId": "com.example.mycameraapp",
+  "appName": "MyCameraApp",
+  "webDir": "dist/my-camera-app",
+  "bundledWebRuntime": false
+};
+
+export default config;
